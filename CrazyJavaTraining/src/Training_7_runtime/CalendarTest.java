9278017f2442cb002d7e44b5901466fee1c34d82
nuts <nuts@wzw-pc>
2017-08-31 21:43:39 +0800

    jixuxuxi

--- CrazyJavaTraining/src/Training_7_runtime/CalendarTest.java
+++ CrazyJavaTraining/src/Training_7_runtime/CalendarTest.java
@@ -3,6 +3,8 @@ package Training_7_runtime;
 import java.awt.CardLayout;
 import java.util.Calendar;
 
+import javax.management.monitor.Monitor;
+
 public class CalendarTest
 {
    public static void main(String [] args)
@@ -23,5 +25,21 @@ public class CalendarTest
 	   //将Calendar 的月前推8个月
 	   calendar.roll(Calendar.MONTH, -8); //2002-03-23 12:32;23
 	   System.out.println(calendar.getTime());
+	   /***add与roll的区别***/
+	   Calendar cal1 = Calendar.getInstance();
+	   cal1.set(2003, 7,23,0,0,0); //2003-8-23
+	   cal1.add(cal1.MONTH, 6); //2004-2-23
+	   
+	   Calendar cal3 = Calendar.getInstance();
+	   cal3.set(2003,7,23,0,0,0);//2003-8-23
+	   //MONTH字段“进位”，但是YEAR字段不增加
+	   cal3.roll(Calendar.MONTH, 6); //2003-8-23->2003-2-23
+	   
+	   Calendar cal4 = Calendar.getInstance();
+	   cal4.set(2003, 7,31,0,0,0); //2003-8-31
+	   //month字段进位后变成2,2月没有31日
+	   //year字段不会改变，2003年2月只有28天
+	   cal4.roll(Calendar.MONTH, 6);
+	   
    }
 }
